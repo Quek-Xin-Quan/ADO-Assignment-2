d41ca48efc4cf1383d18426646c01067d084263a
DarkieDoc <148530888+DarkieDoc@users.noreply.github.com>
2024-01-27 20:31:48 +0800

    edition of documentation

--- Lotso2/models/cleaning/ordercleaning.sql
+++ Lotso2/models/cleaning/ordercleaning.sql
@@ -16,22 +16,22 @@
 --   shippostalcode IS NULL OR 
 --   shipcountry IS NULL;
 
-DELETE FROM raw_orders
-WHERE orderid IS NULL;
+-- DELETE FROM raw_orders
+-- WHERE orderid IS NULL;
 
-UPDATE raw_orders
-SET Shipregion = CASE WHEN Shipregion = 'NULL' THEN 'No Region' ELSE Shipregion END
-WHERE Shipregion = 'NULL';
+-- UPDATE raw_orders
+-- SET Shipregion = CASE WHEN Shipregion = 'NULL' THEN 'No Region' ELSE Shipregion END
+-- WHERE Shipregion = 'NULL';
 
-UPDATE raw_orders
-SET Shippostalcode = CASE WHEN Shippostalcode = 'NULL' THEN 'No Postal' ELSE Shippostalcode END
-WHERE Shippostalcode = 'NULL';
+-- UPDATE raw_orders
+-- SET Shippostalcode = CASE WHEN Shippostalcode = 'NULL' THEN 'No Postal' ELSE Shippostalcode END
+-- WHERE Shippostalcode = 'NULL';
 
 
- UPDATE raw_orders
- SET shippeddate = (
-     SELECT LAG(shippeddate) OVER (ORDER BY orderid)
-     FROM raw_orders AS ro
-     WHERE ro.orderid = raw_orders.orderid
- )
- WHERE shippeddate = '2024-01-01 00:00:00.000';
+--  UPDATE raw_orders
+--  SET shippeddate = (
+--      SELECT LAG(shippeddate) OVER (ORDER BY orderid)
+--      FROM raw_orders AS ro
+--      WHERE ro.orderid = raw_orders.orderid
+--  )
+--  WHERE shippeddate = '2024-01-01 00:00:00.000';
