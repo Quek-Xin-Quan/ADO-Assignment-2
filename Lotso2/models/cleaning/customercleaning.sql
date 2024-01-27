5f9e03bda83bc92ed822a770b14b2644f50fd3e6
Quek-Xin-Quan <131006578+Quek-Xin-Quan@users.noreply.github.com>
2024-01-27 22:04:32 +0800

    changes to cicd

--- Lotso2/models/cleaning/customercleaning.sql
+++ Lotso2/models/cleaning/customercleaning.sql
@@ -1,17 +1,16 @@
 -- --Changing the naming convention of missing values
--- UPDATE raw_customer
--- SET Fax = CASE WHEN Fax = 'NULL' THEN 'No Fax' ELSE Fax END
--- WHERE Fax = 'NULL';
+UPDATE raw_customer
+SET Fax = CASE WHEN Fax = 'NULL' THEN 'No Fax' ELSE Fax END
+WHERE Fax = 'NULL';
 
--- UPDATE raw_customer
--- SET Region = CASE WHEN Region = 'NULL' THEN 'No Region' ELSE Region END
--- WHERE Region = 'NULL';
+UPDATE raw_customer
+SET Region = CASE WHEN Region = 'NULL' THEN 'No Region' ELSE Region END
+WHERE Region = 'NULL';
 
--- DELETE FROM raw_customer
--- WHERE customerid IN (
---      SELECT customerid
---      FROM raw_customer
---      GROUP BY customerid
---      HAVING COUNT(*) > 1
---  );      
+DELETE FROM raw_customer
+WHERE customerid IN (
+          SELECT customerid
+      FROM raw_customer
+      GROUP BY customerid
+      HAVING COUNT(*) > 1  );      
 
